22fd1d7882500596ceead342731e75b619535b95
Alberto Grand <alberto.grand@ircc.it>
2018-02-05 10:40:02 +0100

    -newAnnotationsManager's populate_tables script configured for running in the background; -added empty file funnel-experiments.log in newAnnotationsManager's log folder because code expects it

--- web/start.sh
+++ web/start.sh
@@ -4,7 +4,7 @@
 # for running on the current host
 
 # wait for mysql to be ready then populate annotations tables (will skip import if not first run)
-/las/wait-for-it.sh lasmysql:3306 -s -t 86400 -- /virtualenvs/venvdj1.4/bin/python -u /srv/www/newAnnotationsManager/scripts/populate_tables.py
+/las/wait-for-it.sh lasmysql:3306 -s -t 86400 -- /srv/www/newAnnotationsManager/scripts/populate_tables.sh
 
 # create fqdn.conf
 echo "Define FQDN ${HOST}" >/etc/apache2/fqdn.conf
