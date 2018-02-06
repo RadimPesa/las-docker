75a7e8a2de2e9b70eafb6537f2844c8f3cef3552
Alberto Grand <alberto.grand@ircc.it>
2018-02-06 12:52:49 +0100

    Update .env and set password script

--- web/las/LASAuthServer/set_admin_password.py
+++ web/las/LASAuthServer/set_admin_password.py
@@ -6,6 +6,8 @@ setup_environ(settings)
 from django.contrib.auth.models import User
 
 if __name__ == '__main__':
+    print "Setting password for LAS user `administrator`..."
     u = User.objects.get(username__exact='administrator')
     u.set_password(os.environ['ADMIN_PASSWORD'])
     u.save()
+    print "Done!"
