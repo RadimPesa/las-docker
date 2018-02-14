ce0ffd982e9036d3cfb17e62d431f0f499146ef2
Andrea <14820600+floatingpurr@users.noreply.github.com>
2018-02-14 17:16:50 +0100

    Fix venv paths in createMedicalCenter.sh

--- web/adminScripts/createMedicalCenter.sh
+++ web/adminScripts/createMedicalCenter.sh
@@ -12,6 +12,6 @@ read internalName
 echo
 
 
-~/.virtualenvs/venvdj1.4/bin/python -c "import utils1_4; utils1_4.createSource('$name', 'Hospital', '$internalName')"
+/.virtualenvs/venvdj1.4/bin/python -c "import utils1_4; utils1_4.createSource('$name', 'Hospital', '$internalName')"
 echo
-~/.virtualenvs/venvdj1.7/bin/python -c "import utils1_7; utils1_7.createInstitution('$name', 'Medical Center', '$internalName')"
+/.virtualenvs/venvdj1.7/bin/python -c "import utils1_7; utils1_7.createInstitution('$name', 'Medical Center', '$internalName')"
