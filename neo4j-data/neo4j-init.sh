0ae06314c4bbd8e1e0f809fbfd7557f3078df19c
Alessandro <alessandro.fiori@ircc.it>
2018-05-25 13:23:23 +0200

    update neo4j for node_auto_index

--- neo4j-data/neo4j-init.sh
+++ neo4j-data/neo4j-init.sh
@@ -10,3 +10,22 @@ if [ ! -f /tmp/neo4j-import-done.flag ]; then
 else
     echo "The import has already been made."
 fi
+
+
+setting() {
+    setting="${1}"
+    value="${2}"
+    file="${3}"
+
+    if [ -n "${value}" ]; then
+        if grep --quiet --fixed-strings "${setting}=" conf/"${file}"; then
+            sed --in-place "s|.*${setting}=.*|${setting}=${value}|" conf/"${file}"
+            echo "setting ${setting}=${value}" 
+        else
+            echo "${setting}=${value}" >>conf/"${file}"
+        fi
+    fi
+}
+
+setting "node_auto_indexing" "true" neo4j.properties
+setting "node_keys_indexable" "identifier,genid,WG" neo4j.properties
