7ee09e2671e7278eed483913abbc851bca158d86
Alessandro <alessandro.fiori@ircc.it>
2018-05-28 14:53:27 +0200

    update neo4j init script

--- neo4j-data/neo4j-init.sh
+++ neo4j-data/neo4j-init.sh
@@ -2,16 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# do not run init script at each container strat but only at the first start
-if [ ! -f /tmp/neo4j-import-done.flag ]; then
-    echo "Import database."
-    cp -r /neo4j-data/graph.db /data/
-    touch /tmp/neo4j-import-done.flag
-else
-    echo "The import has already been made."
-fi
-
-
 setting() {
     setting="${1}"
     value="${2}"
@@ -29,3 +19,13 @@ setting() {
 
 setting "node_auto_indexing" "true" neo4j.properties
 setting "node_keys_indexable" "identifier,genid,WG" neo4j.properties
+# do not run init script at each container strat but only at the first start
+if [ ! -f /tmp/neo4j-import-done.flag ]; then
+    echo "Import database."
+    cp -r /neo4j-data/graph.db /data/
+    touch /tmp/neo4j-import-done.flag
+else
+    echo "The import has already been made."
+fi
+
+
