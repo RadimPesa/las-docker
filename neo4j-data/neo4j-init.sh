e122db43bafd42e2f9920e90c9bb7ca094dad4fe
Alessandro <alessandro.fiori@ircc.it>
2018-05-28 15:44:06 +0200

    fix the data import flag

--- neo4j-data/neo4j-init.sh
+++ neo4j-data/neo4j-init.sh
@@ -20,10 +20,10 @@ setting() {
 setting "node_auto_indexing" "true" neo4j.properties
 setting "node_keys_indexable" "identifier,genid,WG" neo4j.properties
 # do not run init script at each container strat but only at the first start
-if [ ! -f /tmp/neo4j-import-done.flag ]; then
+if [ ! -f /data/neo4j-import-done.flag ]; then
     echo "Import database."
     cp -r /neo4j-data/graph.db /data/
-    touch /tmp/neo4j-import-done.flag
+    touch /data/neo4j-import-done.flag
 else
     echo "The import has already been made."
 fi
